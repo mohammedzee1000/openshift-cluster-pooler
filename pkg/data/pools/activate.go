1cade0faea2ff0b475fa01ecd0b55621f67f88c6
Mohammed Zeeshan Ahmed <mohammed.zee1000@gmail.com>
2019-07-23 23:12:17 +0530

    Adding activate function to be used by client api server
    Signed-off-by: Mohammed Zeeshan Ahmed <mohammed.zee1000@gmail.com>

--- pkg/data/pools/activate.go
+++ pkg/data/pools/activate.go
@@ -1,1 +1,67 @@
 package pools
+
+import (
+	"encoding/json"
+	"github.com/dgraph-io/badger"
+	"github.com/mohammedzee1000/openshift-cluster-pool/pkg/config"
+	"github.com/mohammedzee1000/openshift-cluster-pool/pkg/data/database"
+	"github.com/pkg/errors"
+
+	"github.com/mohammedzee1000/openshift-cluster-pool/pkg/data/clusters"
+)
+
+//Activates a cluster, if it is available. This is the only direct db func
+// in pool as we need to ensure cluster activation is a transaction
+func (p Pool) Activate(ctx *config.Context) (*clusters.Cluster, error)  {
+	var c clusters.Cluster
+	var found bool
+	db, err := ctx.NewBadgerConnection()
+	if err != nil {
+		database.HandleError(err)
+	}
+	defer db.Close()
+	// cluster activation transaction
+	err = db.Update(func(txn *badger.Txn) error {
+		//collect all clusters in pool
+		keyprefix := clusters.GetClusterPoolKey(p.Name)
+		it := txn.NewIterator(badger.DefaultIteratorOptions)
+		defer it.Close()
+		prefix := []byte(keyprefix)
+		//iterate over clusters in db, trying to find one which is in success state and can be activated
+		for it.Seek(prefix); it.ValidForPrefix(prefix); it.Next() {
+			item := it.Item()
+			// work with the current item
+			err := item.Value(func(v []byte) error {
+				err = json.Unmarshal(v, &c)
+				if err != nil {
+					return err
+				}
+				// if one of the clusters we are iterating can be activated, activate it and found is true
+				if c.State == clusters.State_Success {
+					found = true
+					c.State = clusters.State_Used
+					data, err := json.Marshal(&c)
+					if err != nil{
+						return err
+					}
+					return txn.Set([]byte(clusters.GetClusterKey(c.ClusterID, p.Name)), data)
+				}
+				return nil
+			})
+			if err != nil {
+				return err
+			}
+			//if we have found what we want, return nil breaking out of the
+			if found {
+				return nil
+			}
+		}
+		return nil
+	})
+	// if not found return an error
+	if !found {
+		return nil, errors.New("could not get a cluster to activae")
+	}
+	database.HandleError(err)
+	return &c, nil
+}
