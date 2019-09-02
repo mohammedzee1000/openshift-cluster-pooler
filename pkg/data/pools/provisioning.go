662eb0b944c28db12865a46ce3d76e6a4bebca84
Mohammed Zeeshan Ahmed <mohammed.zee1000@gmail.com>
2019-09-02 21:48:55 +0530

    Moving deprovisioning logic to seperate file
    Signed-off-by: Mohammed Zeeshan Ahmed <mohammed.zee1000@gmail.com>

--- pkg/data/pools/provisioning.go
+++ pkg/data/pools/provisioning.go
@@ -86,35 +86,6 @@ func (p Pool) provision(ctx *generic.Context) error {
 	return nil
 }
 
-//deprovision destroys clusters resources using uuid and command
-func (p Pool) deprovision(ctx *generic.Context, clusterid string, force bool) error {
-	ctx.Log.Info("Pool deprovision", "deprovisioning clusters of pool %s", p.Name)
-	var cmd string
-	if !force {
-		cmd = p.DeProvisionCommand
-	} else {
-		cmd = p.ForceDeprovisionCommand
-	}
-	c, err := clusters.ClusterByID(ctx, p.Name, clusterid)
-	if err != nil {
-		return err
-	}
-	c.State = clusters.State_DeProvisioning
-	_ = c.Save(ctx)
-	out, err := runCommand(clusterid, cmd)
-	if err != nil {
-		ctx.Log.Error("Pool deprovision", err,"failed to deprovision cluster %s, pool %s", c.ClusterID, p.Name)
-		c.State = clusters.State_Failed
-		_ = c.Save(ctx)
-		PrintIfDebug(ctx.Debug, "deprovision command output", out)
-		return err
-	}
-	c.Delete(ctx)
-	PrintIfDebug(ctx.Debug, "deprovision command output", out)
-	ctx.Log.Info("Pool deprovision", "successfully deprovisioned clusters %s, pool %s", c.ClusterID, p.Name)
-	return nil
-}
-
 //getClusterURL gets the url of specified clusters using provided uuid and command
 func (p Pool) getClusterURL(clusterid string) (string, error) {
 	out, err := runCommand(clusterid, p.ClusterURLCommand)
